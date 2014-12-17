03ae1a743c4f9bc57fbc0b420345127ae01fe8ca
Shibby92 <haris_000@hotmail.com>
2014-12-17 18:23:54 +0100

    v1.01

--- src/Cat.java
+++ src/Cat.java
@@ -1,9 +1,29 @@
+public class Cat extends Zivotinja {
+	private String pasmina;
 
-public class Cat extends Zivotinja{
-	public Cat(String ime,int brNogu){
-		super(ime,brNogu,"macka");
+	public Cat(String ime, int brNogu) {
+		super(ime, brNogu, "macka");
+		this.pasmina = "Grump";
 	}
-	
-	
+
+	public void lajanje() {
+		System.out.println("mjau mjau mjau ");
+	}
+
+	public String getPasmina() {
+		return pasmina;
+	}
+
+	public void setPasmina(String pasmina) {
+		this.pasmina = pasmina;
+	}
+
+	public int testAnimal() {
+		return this.getBrNogu();
+	}
+
+	public String toString() {
+		return "Macka" + this.getIme();
+	}
 
 }
