22161dd093c1cea1acc0950f286c6080561765d9
Shibby92 <haris_000@hotmail.com>
2014-12-19 10:15:34 +0100

    Ispravljene greske
    append..

--- src/Tacka.java
+++ src/Tacka.java
@@ -1,31 +1,41 @@
-
 public class Tacka {
 	private int x;
 	private int y;
-	public Tacka(){
-		this.x=0;
-		this.y=0;
+
+	public Tacka() {
+		this.x = 0;
+		this.y = 0;
 	}
-	public Tacka(int x, int y){
-		this.x=x;
-		this.y=y;
+
+	public Tacka(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
-	public Tacka (Tacka other){
-		this.x=other.x;
-		this.y=other.y;
+
+	public Tacka(Tacka other) {
+		this.x = other.x;
+		this.y = other.y;
 	}
-	public String toString(){
+
+	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("("+this.x+","+this.y+")\n");
+		sb.append("(");
+		sb.append(this.x);
+		sb.append(",");
+		sb.append(this.y);
+		sb.append(")\n");
 		return sb.toString();
 	}
-	public boolean equals (Tacka other){
-		if(this.x==other.x && this.y==other.y)
+
+	public boolean equals(Tacka other) {
+		if (this.x == other.x && this.y == other.y)
 			return true;
 		return false;
 	}
-	public double distance(Tacka b){
-		return Math.sqrt(Math.pow((b.x-this.x), 2)+Math.pow((b.y-this.y),2));
+
+	public double distance(Tacka b) {
+		return Math.sqrt(Math.pow((b.x - this.x), 2)
+				+ Math.pow((b.y - this.y), 2));
 	}
 
 }
